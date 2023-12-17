0c85a249be113d11c38bef9d80dbb444b257508a
Roman Rudin <rudin.roman@bk.rue>
2023-12-17 19:46:26 +0300

    Idea

---
+++ src/components/GameCard.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <image src="https://img.goodfon.ru/original/2880x1800/c/ca/zvezdy-svechenie-tumannosti.jpg"/>
+        <h1>Game</h1>
+        <p>Text</p>
+        
+    </div>
+</template>
