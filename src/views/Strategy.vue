07cb39c51ccb00551de1229c183503e263141123
RomanRudin <rudin.roman@bk.ru>
2023-12-24 19:55:46 +0200

    Views were edited

--- src/views/Strategy.vue
+++ src/views/Strategy.vue
@@ -1,5 +1,28 @@
+<script setup>
+import GameCard from "../components/GameCard.vue";
+</script>
+
 <template>
     <section>
-        <h1>Strategies</h1>
+        <h1 class="title">Strategy</h1>
+        <div class="block">
+            <GameCard v-for="(item, index) in 3" :key="index"/>
+        </div>
     </section>
 </template>
+
+<style scoped>
+.title {
+    font-size: 40px;
+    text-align: center;
+    margin: 20px 0;
+    color: aliceblue;
+}
+.block {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 50px;
+    margin: 0 50px;
+}
+
+</style>
